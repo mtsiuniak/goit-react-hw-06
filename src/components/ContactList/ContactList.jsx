c44b554b8c8d11a51cb615d9526955b472548227
mtsiuniak <mtsuynyak1991@gmail.com>
2024-05-17 10:34:16 -0400

    fix deleteReducer

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,15 +4,14 @@ import { nanoid } from '@reduxjs/toolkit';
 
 export default function ContactList() {
   
-  const contacts = useSelector((state) => state.items.items);
+  const contacts = useSelector((state) => state.items);
   const search = useSelector((state) => state.filters.name);
-  
+  // console.log(contacts);
 
   const filterContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(search.trim().toLowerCase())) 
   
-  
-  
+
   return (
     <>
       {contacts.length !== 0 ? (
