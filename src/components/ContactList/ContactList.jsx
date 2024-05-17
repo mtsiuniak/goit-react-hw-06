913a44d359a57a70a831c5f991e1bf7480c383de
mtsiuniak <mtsuynyak1991@gmail.com>
2024-05-17 08:55:28 -0400

    fix filter issue

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,13 +4,14 @@ import { nanoid } from '@reduxjs/toolkit';
 
 export default function ContactList() {
   
-  const contacts = useSelector((state) => state.items.items);
+  const contacts = useSelector((state) => state.items.contacts);
   const search = useSelector((state) => state.filters.name);
 
 
   const filterContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(search.trim().toLowerCase())) //
-    
+  contact.name.toLowerCase().includes(search.trim().toLowerCase())) 
+  
+  
   return (
     <>
       {contacts.length !== 0 ? (
