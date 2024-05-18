48d6d4e1427e4e1958738236d2bb51201e5a997c
mtsiuniak <mtsuynyak1991@gmail.com>
2024-05-18 04:28:53 -0400

    fixed initial state issue

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,7 +5,7 @@ export default function ContactList() {
   const contacts = useSelector((state) => state.items);
   const search = useSelector((state) => state.filters.name);
 
-  console.log("list", contacts);
+  console.dir("list", contacts);
 
   const filterContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(search.trim().toLowerCase())
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialContacts = [
+const contacts = [
   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
@@ -8,11 +8,9 @@ const initialContacts = [
 ];
 
 const slice = createSlice({
-  name: "contacts",
+  name: "contact",
   initialState: {
-    contacts: {
-      items: initialContacts,
-    }
+    items: contacts,
   },
   reducers: {
     addContact: {
@@ -24,14 +22,14 @@ const slice = createSlice({
           payload: {
             ...values,
             id: crypto.randomUUID(),
-          },
+          }
         };
-      },
+      }
     },
     deleteContact(state, action) {
       return state.filter(contact => contact.id !== action.payload);
-    },
-  },
+    }
+  }
 });
 
 export const { addContact, deleteContact } = slice.actions;
