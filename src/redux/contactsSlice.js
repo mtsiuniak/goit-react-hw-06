407f8179ef608524923978674c06864536c42f75
mtsiuniak <mtsuynyak1991@gmail.com>
2024-05-18 04:15:24 -0400

    changed name for initial state data

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const contacts = [
+const initialContacts = [
   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
@@ -11,7 +11,7 @@ const slice = createSlice({
   name: "contacts",
   initialState: {
     contacts: {
-      items: contacts
+      items: initialContacts,
     }
   },
   reducers: {
